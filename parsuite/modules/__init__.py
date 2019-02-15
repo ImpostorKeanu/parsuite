4be104beea4ee4de53da4f10bd6898b163f5797c
Justin Angel <justinangel86@gmail.com>
2019-02-15 09:26:46 -0500

    restructuring

--- parsuite/modules/__init__.py
+++ parsuite/modules/__init__.py
@@ -1,10 +1,16 @@
-from . import NmapXMLServiceParser
-from . import NmapXMLSMBSecurityMode
-from . import NessusHostDumper 
-from . import URLCrazyToCSV
-from . import ReconNGContactDumper
-from . import NmapTopPortDumper
+#from . import NmapXMLServiceParser
+#from . import NmapXMLSMBSecurityMode
+#from . import NessusHostDumper 
+#from . import URLCrazyToCSV
+#from . import ReconNGContactDumper
+#from . import NmapTopPortDumper
 from sys import modules
+from pathlib import Path
+from importlib import import_module
+
+for f in Path('.').glob('**/*.py'):
+    if p.name.startswith('_'): continue
+    import_module('.'+p.name, 'parsuite')
 
 handles = {
             modn.split('.')[-1]:mod for modn,mod in modules.items()
